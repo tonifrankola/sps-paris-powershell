8cc78c9d9c82c725710de3f988d9de767a81304a
Toni Frankola <toni.frankola@gmail.com>
2017-10-12 10:31:19 +0200

    demos for module 3 and 6

--- Demo3.ps1
+++ Demo3.ps1
@@ -3,3 +3,7 @@
 Install-Module -Name SharePointPnPPowerShellOnline
 Install-Module -Name SharePointDSC
 Install-Module -Name SharePointDSC.Reverse
+
+Get-Module SharePointPnPPowerShell* -ListAvailable | Select-Object Name,Version | Sort-Object Version -Descending
+
+Update-Module SharePointPnPPowerShell*
---
+++ Demo6.ps1
@@ -0,0 +1,24 @@
+Install-Module -Name SharePointPnPPowerShellOnline
+
+Connect-PnPOnline –Url https://M365x167444.sharepoint.com –Credentials (Get-Credential)
+
+$ct = Get-PnPContentType "Document"
+Add-PnPContentType -Name "Project Document" -Description "Use for Contoso projects" -Group "Contoso Content Types" -ParentContentType $ct
+
+Get-PnpTheme
+
+Set-PnPTheme -ColorPaletteUrl /_catalogs/theme/15/palette002.spcolor
+Set-PnPTheme -ColorPaletteUrl /_catalogs/theme/15/palette002.spcolor
+
+Add-PnPNavigationNode -Title "Demo" -Url "/" -Location "QuickLaunch"
+
+New-PnPList -Title Demo -Template Custom
+
+Add-PnPField -List "Demo" -DisplayName "FirstName" -InternalName "FirstName" -Type Text
+Add-PnPField -List "Demo" -DisplayName "LastName" -InternalName "LastName" -Type Text
+
+Add-PnPListItem -List "Demo" -Values @{"Title" = "Mr"; "FirstName" = "Emmanuel"; "LastName" = "Macron"} -ContentType "Item"
+
+https://M365x167444.sharepoint.com
+admin@M365x167444.onmicrosoft.com
+iva.novosel@2468
